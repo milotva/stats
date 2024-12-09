65eb3f0cb3146677b4c8c2bdd7f339cc83bbae93
Milo <milo@milotilo.slmail.me>
2024-12-09 16:24:09 -0400

    publicize exclude list

---
+++ exclude.php
@@ -0,0 +1,33 @@
+<?php
+namespace ext\parser;
+class chkpost {
+public $exclude = [
+	'BanchoBot',
+    'Thorma',
+    'GachaEnjoyer',
+    'gibby10',
+    'sottovoce',
+    '---',
+    'Mother_Nature',
+    'Alisabe',
+    'haxyie',
+    '[NMC]Nijuu',
+    '[LS]Joshstar',
+    'PipeCat',
+    'Supatuka',
+    'WelpImBad',
+	'squishi',
+	'audinor',
+	'Bonessav',
+	'C-OOP',
+	'ananvaban123',
+	'Xovacs',
+    'Nicole_Aquino',
+	'sometimes',
+	'Users',
+	'Disconnected',
+	'Now',
+	'Topic',
+];
+}
+?>
